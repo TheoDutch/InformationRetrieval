a9718bb6ed709ffb9a43125209b9626b46bbd4cc
Theo Pijkeren <tpijkeren@hg137pc25.science.ru.nl>
2018-11-23 12:13:48 +0100

    Indexing documents works now

--- IRsys.py
+++ IRsys.py
@@ -8,21 +8,33 @@ from whoosh import index
 from whoosh.fields import *
 from whoosh.query import *
 import os.path
+from os.path import isfile, join
+from os import listdir
 from whoosh.qparser import QueryParser
+from bs4 import BeautifulSoup
 import codecs
+import frogress
+import re
 
 def get_schema():
-  return Schema(path=ID(unique=True, stored=True), content=TEXT)
+  return Schema(docid=ID(unique=True, stored=True), headline=TEXT(stored=True),
+                path=ID(stored=True), content=TEXT)
+
+def striptags(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
 
 def add_doc(writer, path):
-  #fileobj = open(path, "rb")
-  #content = fileobj.read()
-  #fileobj.close()
-  
-  with codecs.open(path, "r","utf-8") as f:
-      content = f.read()
-  writer.add_document(path=path, content=content)
-
+    infile = open(path,"r").read()
+    infile = '<root>'+infile+'</root>'
+    soup = BeautifulSoup(infile, 'xml')
+    docs  = soup.find_all('DOC')
+    for doc in docs:
+        docid = doc.DOCID.get_text()
+        headline = unicode(striptags(str(doc.HEADLINE)),"utf-8")  # yes, this works, please dont change
+        text = unicode(striptags(str(doc.TEXT)),"utf-8")
+        writer.add_document(docid=docid, headline=headline, path=path, content=text)
+     
 def clean_index(dirname):
   # Always create the index from scratch
   if not os.path.exists(dirname):
@@ -30,14 +42,15 @@ def clean_index(dirname):
   print(dirname, get_schema())
   ix = index.create_in(dirname, get_schema())
   writer = ix.writer()
-
-  doclist = [u'../IRdocs/test1.txt',u'../IRdocs/test2.txt',u'../IRdocs/test3.txt']
+  
+  doclist = [f for f in listdir('../data') if isfile(join('../data', f))]
   # Assume we have a function that gathers the filenames of the
   # documents to be indexed
   #for path in my_docs():
-  for path in doclist:
-    add_doc(writer, path)
-    
+  l = len(doclist)
+  #for i, filename in enumerate(doclist):
+  for filename in frogress.bar(doclist):
+      add_doc(writer, u"../data/" + filename)
   writer.commit()
 
 #==============================================================================
@@ -51,8 +64,8 @@ def clean_index(dirname):
 # writer.commit()
 #==============================================================================
 
-clean_index("newindex")
-ix = index.open_dir("newindex")
+clean_index("data_indexed")
+ix = index.open_dir("data_indexed")
 searcher = ix.searcher()
 parser = QueryParser("content", ix.schema)
 myquery = parser.parse(u"konijn")
