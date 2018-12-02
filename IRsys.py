50bbb290829f68b9ee90d8d3dadbe4ac58ae1a47
KimberlyStoutjesdijk <kimberly.stoutjesdijk@gmail.com>
2018-12-02 16:20:33 +0100

    WIP searcher, indexing fix for my laptop, also added some ideas

--- IRsys.py
+++ IRsys.py
@@ -15,13 +15,21 @@ from bs4 import BeautifulSoup
 import codecs
 import frogress
 import re
+from whoosh import scoring
+from whoosh.lang.porter import stem
 
-datapath = '/mnt/HDD/latimes/'
+#dingen van Theo
+#datapath = '/mnt/HDD/latimes/'
 #datapath = 'testdata/'
 
+#ding van Kimberly
+datapath = "/Users/kimberlystoutjesdijk/Desktop/index/"
+
+laptopVanKimberlyDoetMoeilijk = True
+
 def get_schema():
-        return Schema(docid=ID(unique=True, stored=True),
-        headline=TEXT(stored=True), path=ID(stored=True), content=TEXT)
+    return Schema(docid=ID(unique=True, stored=True),
+    headline=TEXT(stored=True), path=ID(stored=True), content=TEXT)
 
 def striptags(data):
     p = re.compile(r'<.*?>')
@@ -30,13 +38,23 @@ def striptags(data):
 def add_doc(writer, path):
     infile = open(path,"r").read()
     infile = '<root>'+infile+'</root>'
-    soup = BeautifulSoup(infile, 'xml')
-    docs  = soup.find_all('DOC')
-    for doc in docs:
-        docid = unicode(striptags(str(doc.DOCID)), "utf-8")
-        headline = unicode(striptags(str(doc.HEADLINE)),"utf-8")  # yes, this works, please dont change
-        text = unicode(striptags(str(doc.TEXT)),"utf-8")
-        writer.add_document(docid=docid, headline=headline, path=path, content=text)
+    if laptopVanKimberlyDoetMoeilijk:
+        soup = BeautifulSoup(infile, 'html.parser')
+        docs = soup.find_all('doc')
+        for doc in docs:
+            docid = unicode(striptags(str(doc.docid)), "utf-8")
+            headline = unicode(striptags(str(doc.headline)),"utf-8")  # yes, this works, please dont change
+            text = unicode(striptags(str(doc.text)),"utf-8")
+            writer.add_document(docid=docid, headline=headline, path=path, content=text)
+    else:
+        soup = BeautifulSoup(infile, 'xml')
+        docs = soup.find_all('DOC')
+        for doc in docs:
+            docid = unicode(striptags(str(doc.DOCID)), "utf-8")
+            headline = unicode(striptags(str(doc.HEADLINE)), "utf-8")  # yes, this works, please dont change
+            text = unicode(striptags(str(doc.TEXT)), "utf-8")
+            writer.add_document(docid=docid, headline=headline, path=path, content=text)
+
 
 def clean_index(dirname):
     # Always create the index from scratch
@@ -69,12 +87,35 @@ def clean_index(dirname):
 
 #clean_index("latimes_indexed")
 ix = index.open_dir("latimes_indexed")
-searcher = ix.searcher()
-parser = QueryParser("headline", ix.schema)
-myquery = parser.parse(u"television")
-results = searcher.search(myquery)
+# searcher = ix.searcher()
+# parser = QueryParser("content", ix.schema)
+# myquery = parser.parse(u"plants")
+# results = searcher.search(myquery)
+#
+# print(len(results))
+# for result in results:
+#     print(result)
+# searcher.close()
 
-print(len(results))
-for result in results:
-    print(result)
-searcher.close()
+
+#scoring.BM25F(B=0.75, K1=1.5) -> nog uitzoeken wat die parameters precies doen, in de documentation staat alleen 'see literature'
+#scoring.TF_IDF()
+#scoring.Frequency
+queryTest = "plant"
+with ix.searcher(weighting = scoring.Frequency) as searcher:
+    parser = QueryParser("content", ix.schema)
+    myquery = parser.parse(u"" + queryTest)
+    results = searcher.search(myquery)
+
+    #misschien uiteindelijk per query opslaan welke docid's (moeten we deze wel hebben???) zijn gevonden
+
+    #gaat er eigenlijk wel verschil zijn tussen de verschillende weighting dingen? -> staat in de relevantie ook hoe relevant
+    #ze zijn, of alleen dat een doc relevant is?
+    #anders kijken naar de top ... per query?
+
+    print(len(results))
+    for result in results:
+        print(result)
+
+#https://whoosh.readthedocs.io/en/latest/stemming.html
+#^ voor stemming, nog checken of hij dit ook doet voor het indexen
