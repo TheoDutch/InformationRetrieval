e5cba2611a521f7492f8430105563d39e800defb
Tristan Payer <t.payer@gmx.de>
2018-12-07 09:36:24 +0100

    now the extraction of all tags works. (changed topic file)

--- extract_queries.py
+++ extract_queries.py
@@ -5,7 +5,7 @@ import re
 import pandas as pd
 
 # open the file
-infile = open("./data/testset/topics","r").read()
+infile = open("./data/testset/topics.txt","r").read()
 
 # get all the topic numbers
 number_regex = r"\<num> Number: \d{3}"
@@ -18,20 +18,20 @@ for line in infile.splitlines():
     if line.startswith('<title>'):
         topics.append(line.replace('<title> ', ''))
 
-# get the description <- somehow hot working
-#description_regex = r"<desc> Description:([\s\S]*?)<narr>"
-#descriptions = re.findall(description_regex, infile, re.MULTILINE)
-#descriptions = [el.strip() for el in descriptions]        
+# get the description
+description_regex = r"<desc> Description:([\s\S]*?)<narr>"
+descriptions = re.findall(description_regex, infile, re.MULTILINE)
+descriptions = [el.strip() for el in descriptions]        
 
-# get narative <- somehow not working
-#narrative_regex = r"<narr> Narrative:([\s\S]*?)\</top\>"
-#narratives = re.findall(narrative_regex, infile, re.MULTILINE)
-#narratives = [el.strip() for el in narratives]        
+# get narative 
+narrative_regex = r"<narr> Narrative:([\s\S]*?)\</top\>"
+narratives = re.findall(narrative_regex, infile, re.MULTILINE)
+narratives = [el.strip() for el in narratives]        
         
 # convert to pandas df for better overview        
 topic_list = pd.DataFrame(
     {'topic_number': topic_numbers
      ,'title': topics
-     #,'descritptions': descriptions
-     #,'narratives': narratives
+     ,'descritptions': descriptions
+     ,'narratives': narratives
     })
