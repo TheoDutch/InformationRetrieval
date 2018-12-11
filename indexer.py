bfd4441d68348e4cfd52e0ea60a6cc0f68cd94de
Tristan Payer <t.payer@gmx.de>
2018-12-11 23:41:38 +0100

    indexer that indexes all files we need for the TREC evaluation

---
+++ indexer.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 11 17:57:13 2018
+
+@author: tpayer
+"""
+
+import os, os.path
+from whoosh import index
+#import whoosh.index as index
+from whoosh.fields import *
+import glob
+from bs4 import BeautifulSoup
+# import frogress # frogress.bar no longer available??
+from tqdm import tqdm
+from whoosh.writing import AsyncWriter
+import re
+
+def striptags(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data).strip()
+
+# define a schema for the indexer
+def get_schema():
+    return Schema(docno=ID(unique=True, stored=True),
+    headline=TEXT(stored=True), path=ID(stored=True), content=TEXT)
+    
+# get a list with all the documents that have to be indexed
+#here = "."
+#data = "data"
+#all_ = "*"
+
+
+
+#doclist = glob.glob(os.path.join(here, data, "latimes", all_))
+#doclist.extend(glob.glob(os.path.join(here, data, "fbis", all_)))
+#doclist.extend(glob.glob(os.path.join(here, data, "fr94", all_)))
+#doclist.extend(glob.glob(os.path.join(here, data, "ft", all_)))
+
+
+laptopVanKimberlyDoetMoeilijk = False
+def add_doc(writer, path):
+    infile = open(path,"r").read()
+    infile = '<root>'+infile+'</root>'
+    print(path)
+    print(len(infile))
+    soup = BeautifulSoup(infile, 'xml')
+    docs = soup.find_all('DOC')
+    for doc in docs:
+        docno = striptags(str(doc.DOCNO))
+        headline = striptags(str(doc.HEADLINE))  # yes, this works, please dont change <- no it doesn't
+        text = striptags(str(doc.TEXT))
+        writer.add_document(docno=docno, headline=headline, path=path, content=text)
+            
+def index_TREC_ROBUST_04():
+    schema = get_schema()
+    # create an index in the "indexdir" directory.
+    if not os.path.exists("indexdir"):
+        os.mkdir("indexdir")
+        
+    ix = index.create_in("indexdir", schema)
+    ix = index.open_dir("indexdir")
+    
+    # creating the index writer (if fasil use AsyncWriter 
+    # (https://whoosh.readthedocs.io/en/latest/api/writing.html#whoosh.writing.AsyncWriter))
+    # writer = ix.writer()
+    writer = AsyncWriter(ix)
+    
+    dir_list = ['latimes', 'fbis', 'fr94', 'ft']
+    doclist = []
+    [doclist.extend(glob.glob(os.path.join(".", "data", source, "*"))) for source in dir_list]
+    
+    # was frogress.bar instead of tqdm            
+    for filename in tqdm(doclist):
+        add_doc(writer, filename)
+    writer.commit()
+
+
+if __name__ == '__main__':
+    # this function will automatically index the files needed for TREC_Robust_2004
+    index_TREC_ROBUST_04()
+
